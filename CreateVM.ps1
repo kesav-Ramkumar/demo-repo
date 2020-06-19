035544991b46044a4d8697eb0fc279c794bae990
kesav ramkumar <kesav.ramkumar@cognizant.com>
2020-06-19 13:23:42 +0530

    new commit for vm

---
+++ CreateVM.ps1
@@ -0,0 +1,9 @@
+﻿$resourceGroupname = Read-Host -Prompt "enter resource group name"
+
+$adminCreds = Get-Credential -Message "Enter a username and password for the VM administrator."
+
+$vmName = Read-Host -Prompt "enter VM name"
+
+Connect-AzAccount
+
+New-AzVm -ResourceGroupName $resourceGroupname -Name $vmName -Credential $adminCreds -Image UbuntuLTS
